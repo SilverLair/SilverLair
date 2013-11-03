162dbc8d7d2bd3920633275a6f28f6ec41145d9a
SilverLair <silverlairgames@live.com>
2013-11-03 21:37:02 +1100

    Adding main files

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>SilverLair Games &middot; Games of the future</title>
+		<meta name="description" content="SilverLair Games">
+		<meta name="author" content="SilverLair">
+		<meta charset="UTF-8">
+		<link href="assets/styles/silverlair.css" rel="stylesheet">
+	</head>
+	<body>
+		
+	</body>
+	</html>
